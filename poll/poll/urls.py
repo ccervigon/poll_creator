6922b1d5b90b394ef5ffe8f574f730a5fbb48f79
ccervigon <c.cervigonavila@gmail.com>
2014-01-21 15:13:44 +0100

    Added questions.

--- poll/poll/urls.py
+++ poll/poll/urls.py
@@ -8,9 +8,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'pollApp.views.welcome', name='welcome'),
-    url(r'^poll2', 'pollApp.views.poll2', name='poll2'),
-    url(r'^poll', 'pollApp.views.poll', name='poll'),
-    url(r'^thanks', 'pollApp.views.thanks', name='thanks'),
+    url(r'^poll2$', 'pollApp.views.poll2', name='poll2'),
+    url(r'^poll$', 'pollApp.views.poll', name='poll'),
+    url(r'^thanks$', 'pollApp.views.thanks', name='thanks'),
     url(r'^(?P<email_hash>\w+)$', 'pollApp.views.welcome', name='welcome'),
 
     url(r'^admin/', include(admin.site.urls)),
