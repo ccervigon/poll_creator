be6409ce2a036e38f340a4cb03d9b06b16abb166
ccervigon <c.cervigonavila@gmail.com>
2014-01-23 12:32:11 +0100

    DB poll used instead of the DB user. Improvements in email content.

--- sending_mail_to_developers.py
+++ sending_mail_to_developers.py
@@ -2,24 +2,12 @@
 # -*- coding: utf-8 -*- 
 
 import argparse
-import MySQLdb
 import smtplib
 from email.mime.text import MIMEText
+import sqlite3
 
 # Parse command line options
 parser = argparse.ArgumentParser(description="""Scripts to make a poll of specific project""")
-parser.add_argument("-dbhostname",
-                    help = "MySQL hostname",
-                    default = 'localhost')
-parser.add_argument("-dbuser",
-                    help = "MySQL user name",
-                    required = True)
-parser.add_argument("-dbpass",
-                    help = "MySQL password",
-                    required = True)
-parser.add_argument("-dbname",
-                    help = "Name of the database with project cvsanaly information",
-                    required = True)
 parser.add_argument("-email",
                     help="Email",
                     required = True)
@@ -32,36 +20,44 @@ parser.add_argument("-smtp",
 parser.add_argument("-smtp_port",
                     help="SMTP port",
                     required = True)
+parser.add_argument("-poll_dir",
+                    help="Poll directory",
+                    required = True)
+parser.add_argument("-project",
+                    help="Project name",
+                    required = True)
+parser.add_argument("-url",
+                    help="URL where the poll is hosted",
+                    required = True)
 
 args = parser.parse_args()
 
-print 'Project: ' + args.dbname
-con = MySQLdb.connect(host=args.dbhostname, user=args.dbuser, \
-                      passwd=args.dbpass, db=args.dbname)
-con.set_character_set('utf8')
+print 'Project: ' + args.project
+con = sqlite3.connect(args.poll_dir + '/poll/db.sqlite3')
+con.text_factory = lambda x: unicode(x, "utf-8", "ignore")
 cursor = con.cursor()
 
-query = ('SELECT email FROM people WHERE id = ANY (SELECT DISTINCT author_id from scmlog)')
-cursor.execute(query)
-emails = cursor.fetchall()
-
-#Message
-msg = MIMEText("Content")
-msg['Subject'] = 'Subject'
-msg['From'] = args.email
-
-#Authentication
+#Email authentication
 mailServer = smtplib.SMTP(args.smtp,args.smtp_port)
 mailServer.ehlo()
 mailServer.starttls()
 mailServer.ehlo()
 mailServer.login(args.email,args.email_pw)
 
-for email in emails:
-    print 'Sending email to ' + email[0] + '...',
-    msg['To'] = email[0]
-    mailServer.sendmail(args.email, email[0], msg.as_string())
+email_content = 'Example to %s with hash %s/%s' #CHANGE CONTENT
+
+query = ('SELECT name,email,email_hash FROM pollApp_author')
+for developer in cursor.execute(query):
+    print 'Sending email to ' + developer[1] + '...',
+    #Message
+    msg = MIMEText(email_content % (developer[0], args.url, developer[2]))
+    msg['Subject'] = 'Subject'
+    msg['From'] = args.email
+    msg['To'] = developer[1]
+    mailServer.sendmail(args.email, developer[1], msg.as_string())
     print 'OK'
 
 mailServer.close()
+cursor.close()
+con.close()
 print 'Finish'
