a612fdac2130812ee3dec075ab7982a18c565566
ccervigon <c.cervigonavila@gmail.com>
2014-01-28 11:44:58 +0100

    Email content updated with the final text and the libresoft's image.

--- sending_mail_to_developers.py
+++ sending_mail_to_developers.py
@@ -3,7 +3,9 @@
 
 import argparse
 import smtplib
-from email.mime.text import MIMEText
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
 import sqlite3
 
 # Parse command line options
@@ -44,18 +46,40 @@ mailServer.starttls()
 mailServer.ehlo()
 mailServer.login(args.email,args.email_pw)
 
-#INFO: 1º NAME, 2º PROJECT, 3º URL, 4º EMAIL_HASH
-email_content = 'Email to %s in the project %s with hash %s/%s' #CHANGE CONTENT
+#INFO: 1º NAME, 2º PROJECT, 3º URL, 4º EMAIL_HASH, 5º URL
+template_content = '''Hello %s,<br><br>
+
+We are researchers from a Spanish and a British university working on a model for estimating effort in Open Source Software.<br><br>
+
+To validate our findings we have built a small survey -it is composed of 6 questions and should take you less than 3 minutes to respond- about your effort and time spent on the %s project.<br><br>
+
+The survey can be accessed at %s/%s<br><br>
+
+For more information about ourselves, our research, and the possibility to give more feedback, please visit %s<br><br>
+
+Thank you very much,<br><br>
+
+Carlos'''
+
+#Header image
+img_file = open("logo-libresoft.png", "rb")
+attach_image = MIMEImage(img_file.read())
+attach_image.add_header('Content-ID', '<logo-libresoft.png>')
+img_file.close()
 
 query = ('SELECT name,email,email_hash FROM surveyApp_author')
 for developer in cursor.execute(query):
     print 'Sending email to ' + developer[1] + '...',
     #Message
-    #INFO: 1º NAME, 2º PROJECT, 3º URL, 4º EMAIL_HASH
-    msg = MIMEText(email_content % (developer[0], args.project, args.url, developer[2]))
-    msg['Subject'] = 'Subject'
+    msg = MIMEMultipart()
     msg['From'] = args.email
     msg['To'] = developer[1]
+    msg['Subject'] = 'Subject'
+    #INFO: 1º NAME, 2º PROJECT, 3º URL, 4º EMAIL_HASH, 5º URL
+    email_content = template_content % (developer[0], args.project, args.url, developer[2], args.url)
+    msgText = MIMEText('<p>%s</p><br><img src="cid:logo-libresoft.png" width="400" style="display: block;margin: auto;"><br>' % email_content, 'html')
+    msg.attach(msgText)
+    msg.attach(attach_image)
     mailServer.sendmail(args.email, developer[1], msg.as_string())
     print 'OK'
 
