16a669db38e9a017b9d9d9a4dbbaf57d7376bae1
ccervigon <c.cervigonavila@gmail.com>
2014-01-30 13:21:41 +0100

    new version of the HTML pages

--- sending_mail_to_developers.py
+++ sending_mail_to_developers.py
@@ -47,7 +47,7 @@ mailServer.ehlo()
 mailServer.login(args.email,args.email_pw)
 
 #INFO: 1º NAME, 2º PROJECT, 3º URL, 4º EMAIL_HASH, 5º URL
-template_content = '''Hello %s,<br><br>
+template_content = '''Dear %s,<br><br>
 
 We are researchers from a Spanish and a British university working on a model for estimating effort in Open Source Software.<br><br>
 
@@ -55,17 +55,15 @@ To validate our findings we have built a small survey -it is composed of 6 quest
 
 The survey can be accessed at %s/%s<br><br>
 
-For more information about ourselves, our research, and the possibility to give more feedback, please visit %s<br><br>
+For more information about ourselves, our research, and the possibility to give more feedback, please visit %s/contact or find us on the IRC (#libresoft on Freenode).<br><br>
 
-Thank you very much,<br><br>
+Thank you in advance.<br><br>
 
-Carlos'''
-
-#Header image
-img_file = open("logo-libresoft.png", "rb")
-attach_image = MIMEImage(img_file.read())
-attach_image.add_header('Content-ID', '<logo-libresoft.png>')
-img_file.close()
+Carlos Cervigon<br>
+Researcher at Universidad Rey Juan Carlos<br>
+GSyC/LibreSoft Libre Software Engineering Research Lab<br>
+http://www.libresoft.es<br>
+'''
 
 query = ('SELECT name,email,email_hash FROM surveyApp_author')
 for developer in cursor.execute(query):
@@ -74,12 +72,11 @@ for developer in cursor.execute(query):
     msg = MIMEMultipart()
     msg['From'] = args.email
     msg['To'] = developer[1]
-    msg['Subject'] = 'Subject'
+    msg['Subject'] = 'Short survey to tune up Effort Estimation Model for FLOSS'
     #INFO: 1º NAME, 2º PROJECT, 3º URL, 4º EMAIL_HASH, 5º URL
     email_content = template_content % (developer[0], args.project, args.url, developer[2], args.url)
-    msgText = MIMEText('<p>%s</p><br><img src="cid:logo-libresoft.png" width="400" style="display: block;margin: auto;"><br>' % email_content, 'html')
+    msgText = MIMEText('<p>%s</p>' % email_content, 'html')
     msg.attach(msgText)
-    msg.attach(attach_image)
     mailServer.sendmail(args.email, developer[1], msg.as_string())
     print 'OK'
 
